2d094d2174ab19afba476f73b48cd1eb11228aef
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-03-31 21:33:47 +0500

    решение

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,29 @@
 
 int main(){
   std::cout << "Self isolation" << std::endl;
-
+  bool flagError = false;
+  int sumResidents = 0;
+  int countResidents = 0;
+  int residentsFloor = 0;
+  int arr[5] = {};
+  std::cout << "Enter the number of tenants" << std::endl;
+  std::cin >> countResidents;
+  std::cout << "Enter the number of tenants per floors" << std::endl;
+  for(int i = 0; i < 5; i++){
+    std::cin >> residentsFloor;
+    arr[i] = residentsFloor;
+    sumResidents +=residentsFloor;
+  }
+  if(sumResidents > countResidents){
+    std::cout << "The house has more residents than registered " << sumResidents << std::endl;
+    flagError = true;
+  }  
+  if(!flagError){
+    int percentage  = (sumResidents * 100) / countResidents;
+    std::cout << "Percentage of self-isolation " << percentage << std::endl;
+  }
   return 0;
 }
-
 
 // В одной отдельно взятой пятиэтажке проживает N 
 // зарегистрированных жильцов - данное число требуется ввести 
